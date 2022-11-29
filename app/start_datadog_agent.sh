d040ecf6306aa004a2ac094ce7100c498f268e3e
Simon Bélanger <17987029+simonbelanger@users.noreply.github.com>
2022-11-29 11:21:07 -0500

    Use container id as hostname for Datadog agent

--- app/start_datadog_agent.sh
+++ app/start_datadog_agent.sh
@@ -7,6 +7,7 @@ if [ -z "$DD_API_KEY" ]; then
 else
     sudo sh -c "sed -i 's/api_key:.*/api_key: $DD_API_KEY/' /etc/datadog-agent/datadog.yaml"
     sudo sh -c "sed -i 's|SERVICE_NAME|$ROOT_URL|' /etc/datadog-agent/conf.d/nodejs.d/conf.yaml"
+    sudo sh -c "sed -i 's/# hostname:.*/hostname: $GALAXY_CONTAINER_ID/' /etc/datadog-agent/datadog.yaml"
     mkdir /app/logs
     touch /app/logs/current
     datadog-agent run &
